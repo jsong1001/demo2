d991517211095eb0b4be6e4a1cccb38b2c39b736
jsong1001 <jsong_1001@outlook.com>
2023-11-24 10:19:06 +0800

    第一次更新

---
+++ src/githubTest.java
@@ -0,0 +1,11 @@
+/**
+ * @author songjian
+ * @create 2023-11-24-10:13
+ */
+public class githubTest {
+    int id;
+    String name;
+    public void nihao(){
+        System.out.println("nihao");
+    }
+}
